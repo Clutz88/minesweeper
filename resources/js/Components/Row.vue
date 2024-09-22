b0529465d25f28c689e7d85041d494eecba7e7d1
Chris Cutts <chris@chriscutts.co.uk>
2024-09-22 22:57:47 +0100

    Move game logic into view

--- resources/js/Components/Row.vue
+++ resources/js/Components/Row.vue
@@ -1,88 +1,21 @@
 <script setup>
-
 import Cell from "@/Components/Cell.vue";
 
 const props = defineProps(['board', 'row']);
+const emit = defineEmits(['reveal', 'flag'])
 
-function revealAround(cell) {
-    let updates = [];
-    for (let x = -1; x <= 1; x++) {
-        for (let y = -1; y <= 1; y++) {
-            if (cell.y + y > 29) continue;
-            if (cell.y + y < 0) continue;
-
-            updates.push(
-                reveal(
-                    props.board.data.rows[cell.y + y].cells[cell.x + x]
-                )
-            );
-        }
-    }
-    return updates;
+const reveal = (cell) => {
+    emit('reveal', cell);
 }
-
-function sendUpdates(updates) {
-    axios.put(route('board.update', props.board.data), { updates: updates })
-}
-
-const reveal = (cell, initial = false) => {
-    if (typeof cell === "undefined") return;
-    if (cell.is_flag) return;
-
-    if (! cell.is_revealed) {
-        cell.is_revealed = true;
-
-        if (cell.value === "0") {
-            let updates = revealAround(cell);
-            if (initial) {
-                return sendUpdates(updates);
-            }
-            return updates;
-        }
-    }
-
-    if (initial) {
-        sendUpdates([cell])
-    } else {
-        return cell;
-    }
-    return [];
-}
-
-const countFlagsAround = (cell) => {
-    let count = 0;
-    for (let x = -1; x <= 1; x++) {
-        for (let y = -1; y <= 1; y++) {
-            if (cell.y + y > 29) continue;
-            if (cell.y + y < 0) continue;
-            let test_cell = props.board.data.rows[cell.y + y].cells[cell.x + x];
-            if (typeof test_cell !== "undefined") {
-                if (test_cell.is_flag) {
-                    count++;
-                }
-            }
-        }
-    }
-
-    return count;
-}
-
 const flag = (cell) => {
-    if (cell.is_revealed ) {
-        if (countFlagsAround(cell) == cell.value) {
-            sendUpdates(revealAround(cell));
-        }
-        return;
-    }
+    emit('flag', cell);
+}
 
-    cell.is_flag = !cell.is_flag;
-    sendUpdates([cell])
-};
 </script>
 
 <template>
     <div class="flex flex-row">
-        <Cell v-for="cell in row.cells" :key="cell.index" :cell @reveal="reveal(cell, true)" @flag="flag(cell)" />
+        <Cell v-for="cell in row.cells" :key="cell.index" :cell @reveal="reveal(cell)" @flag="flag(cell)" />
     </div>
 </template>
 
