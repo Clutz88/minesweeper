0a6567ce43287172693302a972641070596dc1db
Chris Cutts <chris@chriscutts.co.uk>
2024-09-22 23:03:24 +0100

    Move game logic into composable

---
+++ resources/js/Composables/Minesweeper.js
@@ -0,0 +1,74 @@
+let board;
+let updates = [];
+
+const init = (new_board) => {
+    board = new_board;
+}
+
+const revealAround = (cell) => {
+    for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+            if (cell.y + y > 29) continue;
+            if (cell.y + y < 0) continue;
+            reveal(board.data.rows[cell.y + y].cells[cell.x + x])
+        }
+    }
+}
+
+const sendUpdates = () => {
+    axios.put(route('board.update', board.data), { updates: updates });
+    updates = [];
+}
+
+const reveal = (cell, initial = false) => {
+    if (typeof cell === "undefined") return;
+    if (cell.is_flag) return;
+
+    if (! cell.is_revealed) {
+        cell.is_revealed = true;
+
+        if (cell.value === "0") {
+            revealAround(cell);
+        }
+    }
+    updates.push(cell);
+
+    if (initial) {
+        sendUpdates();
+    }
+    return [];
+}
+
+const countFlagsAround = (cell) => {
+    let count = 0;
+    for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+            if (cell.y + y > 29) continue;
+            if (cell.y + y < 0) continue;
+            let test_cell = board.data.rows[cell.y + y].cells[cell.x + x];
+            if (typeof test_cell !== "undefined") {
+                if (test_cell.is_flag) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
+
+const flag = (cell) => {
+    if (cell.is_revealed ) {
+        if (countFlagsAround(cell) == cell.value) {
+            revealAround(cell);
+            sendUpdates();
+        }
+        return;
+    }
+
+    cell.is_flag = !cell.is_flag;
+    updates.push(cell);
+    sendUpdates()
+};
+
+export { init, reveal, flag };
--- resources/js/Pages/Board/Show.vue
+++ resources/js/Pages/Board/Show.vue
@@ -2,76 +2,14 @@
 
 import Row from "@/Components/Row.vue";
 import GuestLayout from "@/Layouts/GuestLayout.vue";
+import {init, reveal, flag} from "@/Composables/Minesweeper.js"
+import {onMounted} from "vue";
 
-const props = defineProps(['board'])
+const props = defineProps(['board']);
 
-let updates = [];
-
-function revealAround(cell) {
-    for (let x = -1; x <= 1; x++) {
-        for (let y = -1; y <= 1; y++) {
-            if (cell.y + y > 29) continue;
-            if (cell.y + y < 0) continue;
-            reveal(props.board.data.rows[cell.y + y].cells[cell.x + x])
-        }
-    }
-}
-
-function sendUpdates() {
-    axios.put(route('board.update', props.board.data), { updates: updates });
-    updates = [];
-}
-
-const reveal = (cell, initial = false) => {
-    if (typeof cell === "undefined") return;
-    if (cell.is_flag) return;
-
-    if (! cell.is_revealed) {
-        cell.is_revealed = true;
-
-        if (cell.value === "0") {
-            revealAround(cell);
-        }
-    }
-    updates.push(cell);
-
-    if (initial) {
-        sendUpdates();
-    }
-    return [];
-}
-
-const countFlagsAround = (cell) => {
-    let count = 0;
-    for (let x = -1; x <= 1; x++) {
-        for (let y = -1; y <= 1; y++) {
-            if (cell.y + y > 29) continue;
-            if (cell.y + y < 0) continue;
-            let test_cell = props.board.data.rows[cell.y + y].cells[cell.x + x];
-            if (typeof test_cell !== "undefined") {
-                if (test_cell.is_flag) {
-                    count++;
-                }
-            }
-        }
-    }
-
-    return count;
-}
-
-const flag = (cell) => {
-    if (cell.is_revealed ) {
-        if (countFlagsAround(cell) == cell.value) {
-            revealAround(cell);
-            sendUpdates();
-        }
-        return;
-    }
-
-    cell.is_flag = !cell.is_flag;
-    updates.push(cell);
-    sendUpdates()
-};
+onMounted(() => {
+    init(props.board);
+})
 </script>
 
 <template>
